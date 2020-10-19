c7a3548d2341ad5b6789211d60012ec7186b5ec6
Affaf-Istibshar <afafistibsharazhar@gmail.com>
2020-10-18 22:08:15 -0700

    assignment java code

---
+++ AssignOperator.java
@@ -0,0 +1,15 @@
+public class AssignOperator {
+ public static void main(String[] args)
+{ int a,b,c,d,e,f;
+a=10;
+b=5;
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+System.out.println("a+b=" + c);
+System.out.println("a-b=" + d);
+System.out.println("a*b=" + e);
+System.out.println("a/b=" + f); 
+}
+}
