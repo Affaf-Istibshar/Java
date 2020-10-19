161a8a3b74032ae58d9154439f1972a0c66de88d
Affaf-Istibshar <afafistibsharazhar@gmail.com>
2020-10-18 19:02:30 -0700

    While Loop code

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+
+   public static void main(String[] args) {
+      int a = 5;
+
+      while( a < 10 ) {
+         System.out.print("value of x : " + a );
+         a++;
+         System.out.print("\n");
+      }
+   }
+}
